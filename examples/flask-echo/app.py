3a2a599557d0371214426194a61bdd18240b07c9
peco <ppeco85tw@gmail.com>
2023-03-05 07:17:07 +0800

    223

--- examples/flask-echo/app.py
+++ examples/flask-echo/app.py
@@ -1,84 +1,98 @@
-# -*- coding: utf-8 -*-
+# 主要用來接收使用者訊息
 
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
-
-import os
-import sys
-from argparse import ArgumentParser
 
 from flask import Flask, request, abort
+
 from linebot import (
-    LineBotApi, WebhookParser
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    LineBotApiError, InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    TextMessage,ImageMessage,VideoMessage,AudioMessage,LocationMessage,StickerMessage,FileMessage,
+    StickerSendMessage,TextSendMessage,TemplateSendMessage,FlexSendMessage,ImageSendMessage
 )
 
+        
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('b55f6ebc8596d8df0b139f4991851fa7', None)
-channel_access_token = os.getenv('FZT/Idi+V+pcZxmnRfOOKYOzn3nlG6RG9Ohiiys5dqzc03Mqh3f8X/CEfrRzje9uUiy3SaBMVFXe+ua8jusQULhNucGkcKxBxEfVIKSui40Je9L0LVVTwP4sPTYBPvuHCXatUfjO1OXxBflsdXDxUQdB04t89/1O/w1cDnyilFU=', None)
-if channel_secret is None:
-    print('Specify b55f6ebc8596d8df0b139f4991851fa7 as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify FZT/Idi+V+pcZxmnRfOOKYOzn3nlG6RG9Ohiiys5dqzc03Mqh3f8X/CEfrRzje9uUiy3SaBMVFXe+ua8jusQULhNucGkcKxBxEfVIKSui40Je9L0LVVTwP4sPTYBPvuHCXatUfjO1OXxBflsdXDxUQdB04t89/1O/w1cDnyilFU= as environment variable.')
-    sys.exit(1)
+# my token
+line_bot_api = LineBotApi(
+    'FZT/Idi+V+pcZxmnRfOOKYOzn3nlG6RG9Ohiiys5dqzc03Mqh3f8X/CEfrRzje9uUiy3SaBMVFXe+ua8jusQULhNucGkcKxBxEfVIKSui40Je9L0LVVTwP4sPTYBPvuHCXatUfjO1OXxBflsdXDxUQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('b55f6ebc8596d8df0b139f4991851fa7')
 
-line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
+
+@app.route("/")
+def home():
+    # if success
+    return 'home OK'
+
+# 監聽所有來自 /callback 的 Post Request
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # parse webhook body
+    # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='fat')
-        )
 
     return 'OK'
 
 
-if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=8000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
+def replyText_func(user_msg):
+    global replyText
+    
+    replyText = '嗷嗷嗷嗷嗷嗷嗷嗷嗷嗷嗷'
 
-    app.run(debug=options.debug, port=options.port)
+    if '安安' in user_msg:
+        replyText = '安甚麼安'
+    if '胖' in user_msg:
+        replyText = '你最胖'
+    if '令謙' in user_msg:
+        replyText = '老人家'
+    print(replyText)
+    return replyText
+
+
+
+# 處理訊息
+@handler.default()
+def default(event):
+    global replyText
+    
+    # 使用者id
+    sender_id = event.source.user_id
+    # 判別訊息類型
+    msgType = event.message.type
+    
+    if msgType == 'text':
+        # 回復使用者訊息
+        user_msg = event.message.text
+        replyText_func(user_msg)
+        reply = TextSendMessage(text=replyText)
+        line_bot_api.reply_message(event.reply_token,reply)
+    elif msgType == 'sticker':
+        sticker_message = StickerSendMessage(
+            package_id='6359',
+            sticker_id='11069865'
+        )
+        line_bot_api.reply_message(event.reply_token,sticker_message)
+        line_bot_api.push_message(sender_id,TextSendMessage(text='你傳貼圖吼'))
+    else:
+        replyText = '有好康的直接傳給peco(✪ω✪)'
+        reply = TextSendMessage(text=replyText)
+        line_bot_api.reply_message(event.reply_token,reply)
+    
+if __name__ == "__main__":
+    app.run()
